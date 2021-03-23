bff540b7950d0f5d4439f6345a04afb0dd506980
Mykhol <michael@nunc.co.nz>
2021-03-23 14:44:28 +1300

    Add more functionality to getCalendar api method

--- client/src/components/UrlForm.js
+++ client/src/components/UrlForm.js
@@ -1,36 +1,61 @@
 import React from 'react'
-import { useState } from 'react'
+import {useState} from 'react'
 
-export default function UrlForm () {
-  
+export default function UrlForm() {
+
   const [url, setUrl] = useState('')
-  
+  const [calData, setCalData] = useState([])
+
   const urlData = {
     url: url
   }
-  
+
   const handleSubmit = () => {
     console.log('Submitting Request')
     const reqOptions = {
       method: "POST",
-      headers: { 'Content-Type': 'application/json' },
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(urlData),
     }
-    
+
     fetch('/api/test', reqOptions).then(async (r) => {
       const data = await r.json();
       console.log(data)
+      data.sort((a, b) => (a.start > b.start) ? 1 : -1)
+      setCalData(data)
     })
   }
-  
+
   return (
     <div>
       <form>
         <input type={'text'} value={url}
-               onChange={(e) => setUrl(e.target.value)} />
+               onChange={(e) => setUrl(e.target.value)}/>
       </form>
       <button onClick={() => handleSubmit()}>Submit URL</button>
+      <button
+        onClick={() => setUrl('https://uoacal.auckland.ac.nz/calendar/167b0924ba8785a4d69660125665bf00b140d19db8cb7fb94167d9aaeec1ea3f3b95cdfa1cae362da802bd4d232bbfb386c845a592301e89f3b4f924c50966f6')}>Auto
+        URL
+      </button>
+      <div>
+
+        {calData.map((event, index) => {
+          return (
+            <div>
+              <h3>{index}</h3>
+              <p>{event.title}</p>
+              <p>{event.description}</p>
+              <p>{event.location}</p>
+              <p>{new Date(event.start).toString()}</p>
+              <p>{new Date(event.end).toString()}</p>
+              <br/>
+              <br/>
+            </div>
+            )
+        })}
+
+      </div>
     </div>
   )
-  
+
 }
--- server/controllers/DefaultController.js
+++ server/controllers/DefaultController.js
@@ -1,22 +1,53 @@
 // use if requiring a mongo connection
 // const mongoClient = require('../models/MongoConnection');
-const ical = require('node-ical')
+const icsCalendar = require('node-ical')
 
 const getCalendar = async (req, res, next) => {
-  const calendar = await ical.async.fromURL(req.body);
-  const params = [];
-  const events = [];
-  
+
+  const startDate = new Date('March 1, 2021');
+  const calendar = await icsCalendar.async.fromURL(req.body);
+  const allEvents = []
+
   for (const event of Object.values(calendar)) {
     if (event.type === "VEVENT") {
-      events.push(event)
-      console.log(event.description)
-    } else {
-      params.push(event)
+      if (event.start > startDate) {
+        if (!(event.summary.includes("WRK") || event.summary.includes("LAB") || event.summary.includes("CLN"))) {
+          if (event.rrule) {
+
+            // If the event has recurrence rules, then get all occurrences and push them.
+            for (const occurrence of event.rrule.all()) {
+              let duration = event.end - event.start
+
+              let endDate = new Date(occurrence)
+              endDate.setMilliseconds(endDate.getMilliseconds() + duration)
+
+              allEvents.push({
+                title: event.summary,
+                description: event.description,
+                location: event.location,
+                start: occurrence,
+                end: endDate,
+              })
+            }
+          } else {
+
+            // If the event has no rules, push it.
+            allEvents.push({
+              title: event.summary,
+              description: event.description,
+              location: event.location,
+              start: event.start,
+              end: event.end,
+            })
+          }
+        }
+      }
     }
   }
-  
-  res.json(calendar)
+
+  console.log(allEvents)
+
+  res.json(allEvents)
 }
 
 module.exports = {
